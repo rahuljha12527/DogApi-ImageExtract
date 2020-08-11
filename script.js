7087f806f07ddc96dbb726e26fad54eba125e588
rahuljha661 <rahuljha12527@gmail.com>
2020-08-11 14:39:18 +0530

    modified method

--- script.js
+++ script.js
@@ -3,15 +3,36 @@ function fetchRandomDogImage() {
 
   // trueb for asynchronous and false for sychronous
 
-  xhrRequest.onload = function () {
-    console.log(xhrRequest.response);
+  //First Method
 
-    var responseJSON=JSON.parse(xhrRequest.response);
-     var imageUrl=responseJSON.message;
-      $('#dog-image').attr('src',imageUrl);
-  };
-  xhrRequest.open("get", "https://dog.ceo/api/breeds/image/random", true);
-  xhrRequest.send();
+//   xhrRequest.onload = function () {
+//     console.log(xhrRequest.response);
+
+//     var responseJSON=JSON.parse(xhrRequest.response);
+//      var imageUrl=responseJSON.message;
+//       $('#dog-image').attr('src',imageUrl);
+//   };
+//   xhrRequest.open("get", "https://dog.ceo/api/breeds/image/random", true);
+  //xhrRequest.send();
+
+
+  //Second Method
+//   $.ajax({
+//      url:'https://dog.ceo/api/breeds/image/random',
+//      method:'GET',
+//      success:function(data){
+//          console.log(data.message);
+//        var imageUrl=data.message;
+//        $('#dog-image').attr('src',imageUrl);
+//      }
+//   });
+
+//Third Method
+   $.get('https://dog.ceo/api/breeds/image/random',function(data){
+        var imageUrl=data.message;
+        $('#dog-image').attr('src',imageUrl);
+   });
+
 }
 
 $("#fetch-dog-image-button").click(fetchRandomDogImage);
